db98eceb84178043828601de0e6406224405435f
Under20techie <under20techie@gmail.com>
2024-10-15 15:53:55 +0100

    you

--- 0x01-lockboxes/test.py
+++ 0x01-lockboxes/test.py
@@ -3,5 +3,5 @@
 canUnlockAll = __import__('0-lockboxes').canUnlockAll
 
 if __name__ == "__main__":
-  boxes = [[4, 6], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+  boxes = [[0]]
   print(canUnlockAll(boxes))
