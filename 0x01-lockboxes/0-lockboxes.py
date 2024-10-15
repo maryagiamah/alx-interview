131b6ad86db36e745d3f7cb33cc32460db651132
Under20techie <under20techie@gmail.com>
2024-10-15 14:52:57 +0100

    you'

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,11 +3,12 @@
 
 
 def canUnlockAll(boxes):
+    """Write a method that determines if all the boxes can be opened."""
     for i in range(1, len(boxes)):
-        j = i
+        j = i - 1
         flag = 1
-        while j > 0:
-            if i in boxes[j - 1]:
+        while j >= 0:
+            if i in boxes[j]:
                 flag = 0
             j -= 1
     if flag:
