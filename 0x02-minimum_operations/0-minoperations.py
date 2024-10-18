f61869a5c400e6688b27b2e65d606acfbee9b3f5
Under20techie <under20techie@gmail.com>
2024-10-18 04:43:51 +0100

    you

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    no_op = 0
+    div_sor = 2
+
+    while n > 1:
+        while n % div_sor == 0:
+            no_op += div_sor
+            n = n // div_sor
+        div_sor += 1
+
+    return no_op
