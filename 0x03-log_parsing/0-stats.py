9679b75bce8e002c1ca532c46ea414031bac257f
Under20techie <under20techie@gmail.com>
2024-10-25 14:25:38 +0100

    you

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -14,7 +14,7 @@ try:
     for line in sys.stdin:
         pattern = (
                 r"^(\d{1,3}\.?){4} - \[.*?\] "
-                r"\"GET \/projects\/260 HTTP\/1.1\" (\d{0,3}) (\d{1,4})"
+                r"\"GET \/projects\/260 HTTP\/1.1\" (\d{3}) (\d{1,4})"
             )
         match = re.fullmatch(pattern, line.strip())
 
