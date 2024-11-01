5c9032a7040f987704470a2fc4cd805023fa7422
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-11-01 11:40:56 +0100

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,9 +6,20 @@ if a given data set represents a UTF-8 encoding
 
 def validUTF8(data):
     """determines if a given data set represents a valid UTF-8 encoding"""
-    try:
-        byt_str = bytes(data)
-        byt_str.decode("utf-8")
-        return True
-    except (ValueError, UnicodeDecodeError):
-        return False
+    ascii_bit = 1 << 7
+    cont_bit = 0
+    for val in data:
+        ascii_bit = 1 << 7
+        if cont_bit == 0:
+            while val & ascii_bit:
+                ascii_bit >>= 1
+                cont_bit += 1
+            if cont_bit == 0:
+                continue
+            if cont_bit > 4 or cont_bit == 1:
+                return False
+        else:
+            if not ((val & (1 << 7)) and not(val & (1 << 6))):
+                return False
+        cont_bit -= 1
+    return cont_bit == 0
